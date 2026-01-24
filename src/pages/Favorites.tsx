83ced58d81a1e17351f1b7c8f5a8cbb432d988fd
onspace.ai <onspace.ai@gmail.com>
2026-01-24 12:19:25 -0500

    Rename My Favorites tab to My Favs

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -310,7 +310,7 @@ export function Favorites() {
                     : 'text-gray-500 hover:text-gray-700'
                 }`}
               >
-                My Favorites
+                My Favs
               </button>
               <button
                 onClick={() => setActiveTab('points')}
